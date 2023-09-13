6e346ca255b51a87099ca2aceffd4da1d829a93b
Olivia Schif <131719296+livschif@users.noreply.github.com>
2023-09-13 17:31:20 -0500

    defined models to create foreign keys

--- models/index.js
+++ models/index.js
@@ -5,16 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_category',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_category',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    // unique:
+  },
+  as: 'product_tag',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  // unique:
+  },
+  as: 'product_tag'
+});
 
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+module.exports = { Product, Category, Tag, ProductTag };
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,18 +3,40 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// find all tags
+// be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll();
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+// find a single tag by its `id`
+// be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{model: ProductTag, through: Product, as: 'product_tag'}]
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this ID!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
+// create a new tag
 router.post('/', (req, res) => {
-  // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+  }
 });
 
 router.put('/:id', (req, res) => {
